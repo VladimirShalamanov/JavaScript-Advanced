4698f84ab621c5d56ed1d3c9fa58b01e30fa00c8
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-10-07 09:47:28 +0300

    Delete Syntax, Functions and Statements - Lab directory

--- Syntax, Functions and Statements - Lab/01. Echo Function.js
+++
@@ -1,7 +0,0 @@
-function echo(inputAsString){
-    let stringLegth = inputAsString.length;
-    console.log(stringLegth);
-    console.log(inputAsString);
-}
-
-echo('Hello, JavaScript!');
--- Syntax, Functions and Statements - Lab/08. Days in a month.js
+++
@@ -1,5 +0,0 @@
-function solve(month, year){
-    return new Date(year, month, 0).getDate();
-}
-
-console.log(solve(2, 2022));
