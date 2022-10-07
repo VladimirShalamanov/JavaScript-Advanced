c669c1f0f7ba412879cb3fbddb7249db3040b7e1
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2022-10-07 09:49:12 +0300

    Delete Syntax, Functions and Statements - Exercise directory

--- Syntax, Functions and Statements - Exercise/04. Previous Day.js
+++
@@ -1,6 +0,0 @@
-function solve(y, m, d){
-    let currD = new Date(y, m - 1, d - 1);
-    console.log(`${currD.getFullYear()}-${currD.getMonth() + 1}-${currD.getDate()}`);
-}
-
-solve(2016, 10, 1);
--- Syntax, Functions and Statements - Exercise/09. Words Uppercase.js
+++
@@ -1,6 +0,0 @@
-function solve(str) {
-    let words = str.match(/\w+/g).join(", ").toUpperCase();
-    console.log(words);
-}
-
-solve('Hi, how are you?');
